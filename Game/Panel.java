8ddae305ab61b68d307b79bb952efdfefd68172d
HlumaXabanisa <g23x4595@campus.ru.ac.za>
2024-09-16 22:56:03 +0200

    Panel.java

--- Game/Panel.java
+++ Game/Panel.java
@@ -1,21 +1,34 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-class Panel extends JFrame{
+public class Panel {
 	
 		//JFrame frame=new JFrame("Elemental Quest");
 		//JPanel background=new JPanel();
 		//background.setBackground(Color.WHITE);
 		
-		static JFrame frame;
+	//static JFrame frame;
+
 		
-		public static void main(String[] args){
-			frame=new JFrame("Elemental Quest");
-			JPanel background=new JPanel();
-			background.setBackground(Color.CYAN);
-			frame.setSize(1600,1000);
-			frame.show();
-	}
-}
+	public static void main(String[] args){
+		JFrame frame=new JFrame("Elemental Quest");
+			
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1600,800);
+		
+		ImageIcon image1=new ImageIcon("pixe.jpg");
+		JLabel label1=new JLabel(image1);
+		frame.getContentPane().add(label1);
 	
+		//JPanel background=new JPanel();
+		//background.setBackground(Color.red);
+
+			
+			
+		frame.setVisible(true);
+	}
+}	
